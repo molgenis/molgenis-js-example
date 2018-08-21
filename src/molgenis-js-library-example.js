184cdc0ad298e1356a4df3c6be840ce62d74535d
sido <sido@haakma.org>
2018-08-21 15:49:27 +0200

    fix linting

--- src/molgenis-js-library-example.js
+++ src/molgenis-js-library-example.js
@@ -1,6 +1,7 @@
 const exampleMethod = () => {
   let num1 = 1
   let num2 = 2
-  return num1 + num2;
+  return num1 + num2
 }
+
 export default { exampleMethod }
--- test/unit/specs/unit-tests.spec.js
+++ test/unit/specs/unit-tests.spec.js
@@ -3,7 +3,6 @@ import { expect } from 'chai'
 
 describe('Unit test', () => {
   describe('sum', () => {
-
     it('should count the total numbers', done => {
       const result = exampleLibrary.exampleMethod()
       expect(result).to.equal(3)
